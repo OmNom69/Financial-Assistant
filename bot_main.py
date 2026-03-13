e95b1e5f5fd573348db2fe10f37aa6dc6083f5dc
OmNom69 <gosha.novikov.07@mail.ru>
2026-03-13 19:33:58 +0500

    сделал документацию (docs.py) и доделал основные кнопки, также проверку токена

--- bot_main.py
+++ bot_main.py
@@ -2,8 +2,10 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from telegram import ReplyKeyboardMarkup
 from dotenv import load_dotenv
 import os
+import docs
 
 
+'''TOKEN'''
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
@@ -11,14 +13,16 @@ TOKEN = os.getenv("BOT_TOKEN")
 '''KEYBOARD AND MESSAGES'''
 async def start(update, context):
     buttons = [
+        ["ЗАРПЛАТА"],
         ["Еда","Транспорт"],
-        ["Сегодня", "Статистика"],
+        ["Покупки", "Инвестиции"],
+        ["Статистика"],
         ["Помощь"]
     ]
 
     keyboard = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
     await update.message.reply_text(
-        "Привет, я Финансовый помощник!",
+        "Привет, я Финансовый Помощник! Что сегодня сделаем?",
         reply_markup=keyboard
     )
 
@@ -27,36 +31,55 @@ async def help(update, context):
     await update.message.reply_text(
         'Вы можете пользоваться Кнопками! Также есть "Помощь" , там сможете найти все что вам нужно! Но если Кнопки не работают, то ниже можете ознакомиться.\n'
         "========================================\n"
-        "Продукты - запишу сколько вы потратили!\n"
+        "ЗАРПЛАТА - запомню сколько у вас денег!\n"
+        "Еда - запишу сколько вы потратили на вкусняшки!\n"
         "Транспорт - запишу за сколько вы ездили!\n"
-        "Сегодня - посмотрим траты за сегодня!\n"
+        "Покупки - запишу траты!\n"
+        "Инвестиции - запишу куда вы инвестировали!\n"
         "Статистика - посмотрим все траты!\n"
+        "Помощь - расскажу о Командах и Кнопках!\n"
     )
 
 
 async def handle_message(update, context):
     text = update.message.text
 
-    if text == "Продукты":
-        await update.message.reply_text("Запомнил, что вы кушали")
+    if text == "ЗАРПЛАТА":
+        await update.message.reply_text("Запомнил сколько у вас денег!")
+    elif text == "Еда":
+        await update.message.reply_text("Запомнил сколько вы потратили на вкусняшки!")
     elif text == "Транспорт":
-        await update.message.reply_text("Запомнил, за сколько вы ездили")
-    elif text == "Сегодня":
-        await update.message.reply_text("Посмотрим что вы покупали сегодня!")
+        await update.message.reply_text("Запомнил за сколько вы ездили!")
+    elif text == "Покупки":
+        await update.message.reply_text("Запомнил траты!")
+    elif text == "Инвестиции":
+        await update.message.reply_text("Запомнил куда вы инвестировали!")
     elif text == "Статистика":
-        await update.message.reply_text("Посмотрим что вы покупали за все время!")
+        await update.message.reply_text("Посмотрим все траты!")
     elif text == "Помощь":
         await update.message.reply_text(
-            "Продукты - запишу сколько вы потратили\n"
-            "Транспорт - запишу за сколько вы ездили\n"
-            "Сегодня - посмотрим траты за сегодня\n"
-            "Статистика - посмотрим все траты\n"
+            "ПОМОЩЬ\n"
+            "========================================="
+            "ЗАРПЛАТА - запомню сколько у вас денег!\n"
+            "Еда - запишу сколько вы потратили на вкусняшки!\n"
+            "Транспорт - запишу за сколько вы ездили!\n"
+            "Покупки - запишу траты!\n"
+            "Инвестиции - запишу куда вы инвестировали!\n"
+            "Статистика - посмотрим все траты!\n"
         )
     else:
         await update.message.reply_text(f"Я не совсем понял вас: {text}| напишите '/help' ")
 
 
 '''BOT'''
+if __name__ == "__main__":
+    if not TOKEN:
+        print("нет токена")
+        exit()
+
+
+docs.print_docs()  # вывод документации
+
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("help", help))
